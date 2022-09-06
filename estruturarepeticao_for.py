b2d5de30c12e41e118ed65c5917be3a1d78456a4
TarcianoSV <112110052+TarcianoSV@users.noreply.github.com>
2022-09-05 21:22:56 -0300

    Create estruturarepeticao_for.py
    Estrutura de repetição For Basica

---
+++ estruturarepeticao_for.py
@@ -0,0 +1,31 @@
+for c in range (0,6):
+    print(c)
+print('FIM')
+'''sequencia basica numeros'''
+
+
+for c in range (0,6,2):
+    print(c)
+print('FIM')
+'''sequencia basicapulando de 2 em 2'''
+
+
+for c in range (10,0, -1):
+    print(c)
+print ('FIM')
+'''sequencia contagem regressiva'''
+
+n = int(input ('Digite um numero:'))
+for c in range(0,n+1):
+    print(c)
+print('FIM')
+'''sequencia contagem crescente'''
+
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i,f+1, p):
+    print(c)    
+print('FINAL')
+
+'''sequencia controlada com passo a passo controlado'''
