1eb7e4552aad94865693dc67138f5e7bad3d2250
TarcianoSV <112110052+TarcianoSV@users.noreply.github.com>
2022-10-03 14:32:14 -0300

    Create auladelistas.py

---
+++ auladelistas.py
@@ -0,0 +1,24 @@
+'''importar função sleep (tempo de espera) da base de dados padrão (time)'''
+from time import sleep
+
+lista=[]
+dicionario={}
+tuplas=()
+
+'''exemplo de lista'''
+lista=('maria','joana','josefa','toinha','joao','jose','roberto')
+dicionario={}
+
+'''exemplo de impressão de consulta simples'''
+print('mara' in lista)
+
+'''busca por index (posição na lista)'''
+print(lista[0])
+
+'''inserir item na lista (antonio)'''
+lista.append('antonio')
+
+'''remover item na lista (toinha)
+lista.remove('toinha')
+
+lista.sor
